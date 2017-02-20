2b46a895075ddeeb484ed36630358632d5023c05
kmaaser <kmaaser@capital.edu>
2017-02-20 11:34:33 -0500

    starting to write 2 methods

--- inToPost.py
+++ inToPost.py
@@ -5,3 +5,20 @@
 #----------------------------------------------------------------------
 
 from Stack import Stack
+
+def inToPost(s):
+
+    open = 0
+    for ch in s:
+        if ch == '([{':
+            open =+1
+        elif ch == ')]}':
+            open -= 1
+            if open < 0:
+                # there is no matching opener, so check fails
+                return False
+    return open == 0 # everything balances if no unmatched opens
+
+def evalPostfix:
+
+    pass
