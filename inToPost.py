498f2e062a1444e964c306d87a2639ef0d638bc9
kmaaser <kmaaser@capital.edu>
2017-02-24 00:18:52 -0500

    added comments

--- inToPost.py
+++ inToPost.py
@@ -13,6 +13,8 @@ def inToPost(infixExpression):
     stack = Stack()
     postfixExpression = ""
     operator = ["*", "/", "+", "-"]
+
+    # precedence
     importance = {"*": 1,
                   "/": 1,
                   "+": 0,
@@ -20,45 +22,53 @@ def inToPost(infixExpression):
 
     # splits the expression at the spaces
     newExpression = infixExpression.split()
-    print(newExpression)
 
     # goes through the expression
     for token in newExpression:
-        print (postfixExpression)
-        print (stack.items)
         if token.replace(".","",1).isdigit():
             postfixExpression += (token+" ")
+        # if the token is a left parentheses
         elif token in ["(", "[", "{"]:
             stack.push(token)
         # if the token is in the operator
         elif token in operator:
+            # checks the size
+            # checks what is on the top of the stack
+            # checks the importance of the top of stack vs the token
             while stack.size() != 0 and stack.top() in "*/+-" and (importance[stack.top()] >= importance[token]):
                 postfixExpression += stack.pop() + " "
             stack.push(token)
 
         else:
+            # if the top of the stack is a left parentheses
             while stack.top() not in ["(", "[", "{"]:
-                print(stack.items)
                 postfixExpression += stack.pop() + " "
             stack.pop()
     postfixExpression+= stack.pop()+" "
+
     while stack.size()!=0:
         postfixExpression += stack.pop() + " "
-    print (postfixExpression)
+    # returns the postExpression
     return postfixExpression
-
+#----------------------------------------------------------------------
 
 def evalPostfix(postfixExpression):
 
+    # creates an empty stack
+    # keeps track of answer
     numStack = Stack()
     answer = 0
     operator = ["*", "/", "+", "-"]
+    # splits the postExpression
     postList = postfixExpression.split()
 
     for token in postList:
         if token in operator:
+            # pops two numbers off of the stack
             item1 = eval(numStack.pop())
             item2 = eval(numStack.pop())
+            # performs the correct operation depending on what operator is is
+            # then pushes the answer back on the stack
             if token == "+":
                 answer = (item1 + item2)
                 numStack.push(str(answer))
@@ -71,7 +81,9 @@ def evalPostfix(postfixExpression):
             elif token == "/":
                 answer = (item2 / item1)
                 numStack.push(str(answer))
+        # if the token is not a operator
         else:
             numStack.push(token)
     answer = eval(numStack.pop())
+    # returns the "answer"
     return answer
